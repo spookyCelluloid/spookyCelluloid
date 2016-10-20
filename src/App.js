176618a7f9e187199b453fb4c1cd4d059331dc9a
Hack Reactor Students <accounts@hackreactor.com>
2016-10-19 17:29:13 -0700

    Create new component files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Mission from './Mission';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App Row container-fluid row row-centered">
+        <div className="App-header col-md-8 text-center" style={{'border':'3px solid orange'}}>
+          <Header />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col-md-8" style={{'border':'3px solid blue'}}>
+          <Mission />
+        </div>
+        <div className="col-md-8">
+          <h1 style={{'border':'3px solid red'}}>Search Bar</h1>
+        </div>
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <h1>Nursing Home App</h1>
+    )
+  }
+}
+
+export default Header;
