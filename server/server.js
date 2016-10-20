a74e9794d170177b4567d3c8f5f7fc134c082219
Hack Reactor Students <accounts@hackreactor.com>
2016-10-20 12:27:35 -0700

    Add post request to SQL with mock data.

--- server/server.js
+++ server/server.js
@@ -12,8 +12,32 @@ app.get('/', function(req, res) {
   res.send('hello');
 });
 
-app.post('/', function(req, res) {
-  res.send('post hello');
+app.post('/businessprofile', function(req, res) {
+  var ownership = req.body.ownership;
+  var dietary_options = req.body.dietary_options;
+  var social_events = req.body.social_events;
+  var number_of_staff = req.body.number_of_staff;
+  var capacity = req.body.capacity;
+  var Medicare = req.body.Medicare;
+  var average_years_of_experience = req.body.average_years_of_experience;
+  var facility_name = req.body.facility_name;
+  var description = req.body.description;
+  var data = {
+    ownership: ownership,
+    dietary_options: dietary_options,
+    social_events: social_events,
+    number_of_staff: number_of_staff,
+    capacity: capacity,
+    Medicare: Medicare,
+    average_years_of_experience: average_years_of_experience,
+    facility_name: facility_name,
+    description: description
+  }
+
+  connection.query('SELECT * from Business_profile', function(err, rows, fields) {
+    res.json(data);
+  });
+
 });
 
 
@@ -22,3 +46,4 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log('listening on port ' + port);
 });
+
