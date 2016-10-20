ea11bf6dddacd5ecab55825fd286a8213b69c84d
Hack Reactor Students <accounts@hackreactor.com>
2016-10-20 11:44:15 -0700

    Implement React router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Mission from './Mission';
-
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory} from 'react-router'
+import Header from './Header'
+import Search from './Search'
+import Mission from './Mission'
 
 class App extends Component {
   render() {
     return (
-      <div className="App Row container-fluid row row-centered">
-        <div className="App-header col-md-8 text-center" style={{'border':'3px solid orange'}}>
-          <Header />
-        </div>
-        <div className="col-md-8" style={{'border':'3px solid blue'}}>
-          <Mission />
-        </div>
-        <div className="col-md-8">
-          <h1 style={{'border':'3px solid red'}}>Search Bar</h1>
-        </div>
-      </div>
+      <Router history={browserHistory}>
+        <Route path='/' component={Header}/>
+        <Route path='/header' component={Header}/>
+        <Route path='/search' component={Search}/>
+        <Route path='/mission' component={Mission}/>
+      </Router>
     );
   }
 }
