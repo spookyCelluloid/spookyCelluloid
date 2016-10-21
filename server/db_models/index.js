a3400d3c8e63993ec203f1d0bd53ba1a31aa8659
Hack Reactor Students <accounts@hackreactor.com>
2016-10-20 19:44:31 -0700

    Create query for facilityProfile.

--- server/db_models/index.js
+++ server/db_models/index.js
@@ -4,8 +4,8 @@ module.exports = {
   facility: {
     getFacilityList: function(callback) {
       //queries the database for all nursing homes  
-      var facilityListQuery = 'SELECT b.id, b.facility_name, b.phone_number, b.address, b.Medicare, b.image_url, AVG(c.rating) AS average_rating, COUNT(c.rating) AS num_ratings ' + 
-                              'FROM Business_profile b JOIN Comments c ' + 
+      var facilityListQuery = 'SELECT b.id, b.facility_name, b.phone_number, b.street, b.city, b.state, b.zip, b.Medicare, b.image_url, AVG(c.rating) AS average_rating, COUNT(c.rating) AS num_ratings ' + 
+                              'FROM Business_profile b JOIN Reviews c ' + 
                               'ON (b.id = c.id_business_profile) ' +
                               'GROUP BY b.id';
                 
@@ -27,6 +27,8 @@ module.exports = {
             callback(err);
             return;
           }
+
+         
           //iterate through the array of facility objects, adds a key called specialities and adds in an array of speciality values
           for (var i = 0; i < listResults.length; i++) {
             listResults[i].specialites = ratingResults.filter(function(rating) {
@@ -40,6 +42,56 @@ module.exports = {
         });
 
       });
+    },
+
+    getFacilityProfile: function(facilityID, callback) {
+
+      var FacilityProfileQuery = 'SELECT * FROM Business_profile WHERE id = ' + facilityID;
+
+      db.query(FacilityProfileQuery, function(err, business){
+        if (err) {
+          console.log('Error with getFacilityProfile query');
+          callback(err);
+          return;
+        } 
+
+        var specialtiesQuery = 'SELECT s.name ' +
+                               'FROM Specialties s JOIN Business_specialties bs ' +
+                               'ON (bs.id_specialties = s.id) ' +
+                               'WHERE bs.id_business_profile = ' + facilityID;
+
+        db.query(specialtiesQuery, function(err, specialties) {
+          if (err) {
+            console.log('error with specialtiesQuery');
+            callback(err);
+            return;
+          }
+
+          var reviewsQuery = 'SELECT r.content, r.date, r.rating, u.first_name, u.last_name ' +
+                             'FROM Reviews r JOIN User u ' +
+                             'ON (u.id = r.id_User) ' +
+                             'WHERE r.id_business_profile = ' + facilityID;
+
+          db.query(reviewsQuery, function(err, reviews) {
+            if (err) {
+              console.log('error with reviewsQuery');
+              callback(err);
+              return;
+            }
+
+            var profile = business[0];
+            
+            profile.specialties = specialties.map((specialty) => specialty.name);
+            profile.reviews = reviews.filter((review) => review.content !== undefined);
+
+            callback(null, profile);
+          })
+
+
+        });
+
+      })
+
     }
   }
 }
