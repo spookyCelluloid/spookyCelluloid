4f474fc6b321ce099b2daa8cca03c676bf0470a7
Hack Reactor Students <accounts@hackreactor.com>
2016-10-26 15:32:48 -0700

    work on facility profile, build star system

--- client/src/FacilityProfile.js
+++ client/src/FacilityProfile.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import StarRating from 'react-star-rating';
 require('./facilityProfile.css');
+
+
 
 const FacilityProfile = ({currentProfile}) => (
 
   <div>
     <div className='title'>
-      <h2 className='facilityName'>{currentProfile.facility_name}: {currentProfile.reviews[0].rating} stars</h2>
+        <h2 className='facilityName'>{currentProfile.facility_name} <StarRating name='mainRating' rating={4}/></h2>
     </div>
     <div className='mainImage'>
-      <img src='http://www.nursinghomes.com/wp-content/tools/TimThumb.php?src=/wp-content/uploads/property-images/055107/055107__4.jpg&w=800&h=800zc=3'/>
+      <div className='imageBorder'>
+        <img src='http://www.nursinghomes.com/wp-content/tools/TimThumb.php?src=/wp-content/uploads/property-images/055107/055107__4.jpg&w=800&h=800zc=3'/>
+      </div>
     </div>
-    <div className='column'>
+    <div>
+      <p>{console.log(currentProfile)}</p>
       <div className='description'>
         <p><strong>Description: </strong>{currentProfile.description}</p>
       </div>
@@ -27,7 +33,10 @@ const FacilityProfile = ({currentProfile}) => (
       {currentProfile.reviews.map((review) => {
         return (
           <div>
-            <p className='reviewName'>{review.first_name} {review.last_name} {review.rating} stars</p>
+            <p className='reviewName'>{review.first_name} {review.last_name}</p>
+            <div className='starRating'>
+              <StarRating name='starRating' rating={review.rating}/>
+            </div>
             <p className='reviewDate'>Date {review.date}</p>
             <p className='reviewContent'><strong>Review: </strong>{review.content}</p>
           </div>
