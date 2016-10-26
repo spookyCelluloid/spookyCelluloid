2e0e59fd078f2b219475ce98adebce0463525d54
Hack Reactor Students <accounts@hackreactor.com>
2016-10-26 13:59:19 -0700

    facility profile css

--- client/src/FacilityProfile.js
+++ client/src/FacilityProfile.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
-
+require('./facilityProfile.css');
 
 const FacilityProfile = ({currentProfile}) => (
 
   <div>
-    <div>{currentProfile.facility_name} </div>
-    <div>{console.log(currentProfile)}</div>
-    <div>
-      <h2>{currentProfile.facility_name}</h2>
-      <h2>{currentProfile.reviews[0].rating}</h2>
+    <div className='title'>
+      <h2 className='facilityName'>{currentProfile.facility_name}: {currentProfile.reviews[0].rating} stars</h2>
     </div>
-    <div>
+    <div className='mainImage'>
       <img src='http://www.nursinghomes.com/wp-content/tools/TimThumb.php?src=/wp-content/uploads/property-images/055107/055107__4.jpg&w=800&h=800zc=3'/>
     </div>
-    <div>
-      <p>{currentProfile.description}</p>
-    </div>
-    <div>
-      <p>{currentProfile.phone_number}</p>
-      <p>{currentProfile.street}</p>
-      <p>{currentProfile.city}</p>
-      <p>{currentProfile.state}</p>
-      <p>{currentProfile.zip}</p>
-    </div>
-    <div>
+    <div className='column'>
+      <div className='description'>
+        <p><strong>Description: </strong>{currentProfile.description}</p>
+      </div>
+      <div className='contactInfo'>
+      <p><strong>Contact Information: </strong></p>
+        <p className='phoneNumber'>{currentProfile.phone_number}</p>
+        <p className='street'>{currentProfile.street}</p>
+        <p className='city'>{currentProfile.city}</p>
+        <p className='state'>{currentProfile.state}</p>
+        <p className='zip'>{currentProfile.zip}</p>
+      </div>
+    </div> 
+    <div className='reviews'>
       {currentProfile.reviews.map((review) => {
         return (
           <div>
-            <p>{review.first_name}</p>
-            <p>{review.last_name}</p>
-            <p>{review.rating}</p>
-            <p>{review.date}</p>
-            <p>{review.content}</p>
+            <p className='reviewName'>{review.first_name} {review.last_name} {review.rating} stars</p>
+            <p className='reviewDate'>Date {review.date}</p>
+            <p className='reviewContent'><strong>Review: </strong>{review.content}</p>
           </div>
         )
       })}
